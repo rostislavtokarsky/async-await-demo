2b3ab0344cedd5407ade565db33a868d77f3451f
Rostislav Tokarsky <rostislav.tokarsky@gmail.com>
2022-07-20 15:39:37 +0200

    space

--- Shared/Example.cs
+++ Shared/Example.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Threading;
-
+ 
 namespace Shared
 {
     public class Example
